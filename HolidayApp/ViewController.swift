f97f36be9e9fcd9fafbf3f6968c6e1bea83a172d
Kwame <kwameagyenimboateng64@gmail.com>
2021-07-28 01:50:19 +0000

    Initial Commit

---
+++ HolidayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HolidayApp
+//
+//  Created by Kwame Agyenim - Boateng on 28/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
