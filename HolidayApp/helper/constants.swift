4b96ce207dd3f4f66dcfe3fc5aa8581ce86c7ecc
Kwame <kwameagyenimboateng64@gmail.com>
2021-08-06 01:30:51 +0000

    added  public APIs

---
+++ HolidayApp/helper/constants.swift
@@ -0,0 +1,16 @@
+//
+//  constants.swift
+//  HolidayApp
+//
+//  Created by Kwame Agyenim - Boateng on 05/08/2021.
+//
+
+import Foundation
+
+
+struct Constants {
+    struct API {
+        static let holiday_api_key = "4f5a8dd7b979c27a214e0e8503b0efe9dedbf0de"
+        static let country_api = "https://restcountries.eu/rest/v2/all"
+    }
+}
